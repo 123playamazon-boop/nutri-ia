05c56f906d4094eac7537483efdea26548e5269f
123playamazon-boop <123playamazon@gmail.com>
2026-06-09 21:55:51 -0300

    Add UTMify tracking scripts (UTM capture + pixel)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script id="fb-pixel" strategy="afterInteractive">
           {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','2060375741236309');fbq('track','PageView');`}
         </Script>
+        <Script id="utmify-utms" strategy="afterInteractive">
+          {`(function(){var u=document.createElement("script");u.setAttribute("src","https://cdn.utmify.com.br/scripts/utms/latest.js");u.setAttribute("data-utmify-prevent-xcod-sck","");u.setAttribute("data-utmify-prevent-subids","");u.async=true;u.defer=true;document.head.appendChild(u);})();`}
+        </Script>
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`window.pixelId="6a28b28f84fac4cd8ad90547";(function(){var a=document.createElement("script");a.setAttribute("async","");a.setAttribute("defer","");a.setAttribute("src","https://cdn.utmify.com.br/scripts/pixel/pixel.js");document.head.appendChild(a);})();`}
+        </Script>
         <div className="min-h-screen bg-background">{children}</div>
       </body>
     </html>
